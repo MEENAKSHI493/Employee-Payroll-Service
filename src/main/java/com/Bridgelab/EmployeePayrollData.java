5de638c13b287b1c5dae4a5d659978ebbda0bbc0
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-08-10 23:40:45 +0530

    create employee payrole service to read and write a data in console

--- src/main/java/com/Bridgelab/EmployeePayrollData.java
+++ src/main/java/com/Bridgelab/EmployeePayrollData.java
@@ -1,4 +1,37 @@
 package com.Bridgelab;
-
+   /**
+    * @ authour Meenakshi
+    * Create employee Payroll data
+    */
 public class EmployeePayrollData {
+    private String name;
+    private int id;
+    private  double salary;
+
+     /**
+      *  constructor Employee Payroll data
+      */
+    public EmployeePayrollData(String name, int id, double salary) {
+        super();
+        this.name = name;
+        this.id = id;
+    }
+    public  String getName() {
+
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public int getId()
+    {
+        return id;
+    }
+    public double getSalary() {
+        return salary;
+    }
+
+    public static void add(EmployeePayrollData employeePayrollData){
+}
 }
--- src/main/java/com/Bridgelab/EmployeePayrollService.java
+++ src/main/java/com/Bridgelab/EmployeePayrollService.java
@@ -1,7 +1,56 @@
 package com.Bridgelab;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+    /**
+     *  create a  employee payrollservice class
+     *  create arrylist and list to  collect and read data.
+     *  read and write Employee Payroll to a Console
+     */
+
 public class EmployeePayrollService {
-    public static  void main (String[] args){
-        System.out.println("Welcome to employeepayrollService");
+
+    public enum IOService { CONSOLE_IO,FILE_ID,DB, REST_IO}
+    private List<EmployeePayrollData> employeePayrollist;
+
+    private ArrayList<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
+    public  EmployeePayrollService() {
     }
+    public  EmployeePayrollService(List<EmployeePayrollData> employeePayrollDataList2) {
+        this.employeePayrollDataList = (ArrayList<EmployeePayrollData>) employeePayrollDataList2;
+    }
+
+        /**
+         * Mian method
+         * @param args calling method
+         */
+        public static  void main (String[] args){
+            System.out.println("Welcome to employeepayrollService");
+            ArrayList<EmployeePayrollData> employeePayrollDataList = new ArrayList<EmployeePayrollData>();
+            EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollDataList);
+            Scanner sc = new Scanner(System.in);
+            employeePayrollService.readEmployeePayrollData(sc);
+            employeePayrollService.writeEmployeePayrollData();
+        }
+         /**
+          * read employee payroll data using console
+          */
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Please enter Employee ID:");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Please enter the Employee name");
+        String name = consoleInputReader.next();
+        System.out.println("Please enter the Employee salary");
+        double salary = consoleInputReader.nextDouble();
+        EmployeePayrollData.add(new EmployeePayrollData(name, id, salary));
+    }
+        /**
+         *  write Writing payroll to console
+         */
+    private void writeEmployeePayrollData() {
+        System.out.println(("Writing payroll to console\n" + employeePayrollDataList));
+        }
+
+
 }
